932b991c8ea1005c51d702e889fbfbf7108dfe9c
Basit <bta.aang@gmail.com>
2024-12-22 00:30:42 +0700

    Initial Commit

---
+++ Go Plan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Go Plan
+//
+//  Created by Basit Tri Anggoro on 22/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
